2a18d003115e59f658d5e00fca7db4be699f1f34
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-27 11:39:35 +0700

    Update TotaaTeamServiceProvider.php

--- src/TotaaTeamServiceProvider.php
+++ src/TotaaTeamServiceProvider.php
@@ -17,7 +17,7 @@ class TotaaTeamServiceProvider extends ServiceProvider
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'totaa-team');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'totaa-team');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
