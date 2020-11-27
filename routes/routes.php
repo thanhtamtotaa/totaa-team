f3580d7d816401b70b19cd906a862e1008814980
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-27 11:39:43 +0700

    Create routes.php

---
+++ routes/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Totaa\TotaaTeam\Http\Controllers\TeamController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware(['web', 'auth', 'CheckAccount'])->group(function () {
+
+    //Route Admin
+    Route::redirect('admin', '/', 301);
+    Route::group(['prefix' => 'admin'], function () {
+
+        //Route quản lý thành viên
+        Route::redirect('member', '/', 301);
+        Route::group(['prefix' => 'member'], function () {
+
+            Route::get('team',  [TeamController::class, 'index'])->name('admin.member.team');
+
+        });
+
+    });
+
+});
