fdea69f08c369989019ee5c0fa5fa094b8502e09
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-27 16:50:22 +0700

    Update team-livewire.blade.php

--- resources/views/livewire/team-livewire.blade.php
+++ resources/views/livewire/team-livewire.blade.php
@@ -1,13 +1,13 @@
 <div>
     <!-- Filters and Add Buttons -->
-    @include('totaa-team::livewire.team.support.filters')
+    @include('totaa-team::livewire.support.filters')
 
     <!-- Incluce các modal -->
-    @include('totaa-team::livewire.team.modal.add_edit')
-    @include('totaa-team::livewire.team.modal.delete_modal')
+    @include('totaa-team::livewire.modal.add_edit_modal')
+    @include('totaa-team::livewire.modal.delete_modal')
 
     <!-- Scripts -->
     @push('livewires')
-        @include('totaa-team::livewire.team.support.script')
+        @include('totaa-team::livewire.support.script')
     @endpush
 </div>
