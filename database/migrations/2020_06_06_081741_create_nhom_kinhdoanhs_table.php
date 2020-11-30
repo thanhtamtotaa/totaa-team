75b0770e79ce3781153b6b1061e097d4e25ffc36
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-30 10:29:08 +0700

    Update 2020_06_06_081741_create_nhom_kinhdoanhs_table.php

--- database/migrations/2020_06_06_081741_create_nhom_kinhdoanhs_table.php
+++ database/migrations/2020_06_06_081741_create_nhom_kinhdoanhs_table.php
@@ -16,7 +16,7 @@ class CreateNhomKinhdoanhsTable extends Migration
         Schema::create('nhom_kinhdoanhs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100);
-            $table->bigInteger('team_type_id')->unsigned()->nullable();
+            $table->bigInteger('kenh_kd_id')->unsigned()->nullable();
             $table->integer('order')->nullable()->default(null);
             $table->boolean('active')->nullable()->default(null);
             $table->unsignedBigInteger('created_by')->nullable()->default(null);
@@ -24,7 +24,7 @@ class CreateNhomKinhdoanhsTable extends Migration
             $table->unsignedBigInteger('deleted_by')->nullable()->default(null);
             $table->timestamps();
             $table->softDeletes();
-            $table->foreign('team_type_id')->references('id')->on('team_types')->onDelete('SET NULL')->onUpdate('cascade');
+            $table->foreign('kenh_kd_id')->references('id')->on('kenh_kinhdoanhs')->onDelete('SET NULL')->onUpdate('cascade');
         });
     }
 
