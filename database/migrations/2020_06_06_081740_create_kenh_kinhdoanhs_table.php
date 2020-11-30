c2fa99a6961c24729f329c32336760e089f95b04
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-30 10:29:06 +0700

    Create 2020_06_06_081740_create_kenh_kinhdoanhs_table.php

---
+++ database/migrations/2020_06_06_081740_create_kenh_kinhdoanhs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kenh_kinhdoanhs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->boolean('active')->nullable()->default(null);
+            $table->unsignedBigInteger('created_by')->nullable()->default(null);
+            $table->unsignedBigInteger('updated_by')->nullable()->default(null);
+            $table->unsignedBigInteger('deleted_by')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kenh_kinhdoanhs');
+    }
+}
