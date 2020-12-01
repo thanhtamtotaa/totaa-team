2a5e33dedca6e1fae7ceb668b18cd6728f2decaa
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-01 08:18:47 +0700

    Create style.blade.php

---
+++ resources/views/livewire/support/style.blade.php
@@ -0,0 +1,8 @@
+<style>
+    [totaa-datatables]#team-table > thead > tr > th:nth-child(5),
+    [totaa-datatables]#team-table > tbody > tr > td:nth-child(5) {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        max-width: 230px;
+    }
+</style>
